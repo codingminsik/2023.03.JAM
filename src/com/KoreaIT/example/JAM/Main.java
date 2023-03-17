8c5e10ec2e2ed8f8154bb6d5943e25586aace493
codingminsik <codingminsik@gmail.com>
2023-03-17 16:54:40 +0900

    스프링

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -1,45 +1,56 @@
 package com.KoreaIT.example.JAM;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("프로그램 시작");
+		System.out.println("=프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
+
 		List<Article> articles = new ArrayList<>();
 		int lastArticleId = 0;
-		
-		while(true) {
-			
+
+		while (true) {
 			System.out.print("명령어 ) ");
 			String cmd = sc.nextLine().trim();
-		
-			if(cmd.equals("exit")) {
+
+			if (cmd.equals("exit")) {
 				break;
 			}
-		
-			if(cmd.equals("article write")) {
-				System.out.println("게시글 작성");
+
+			if (cmd.equals("article write")) {
+				System.out.println("==게시물 작성==");
 				int id = lastArticleId + 1;
-				System.out.print("제목 ) ");
+				System.out.printf("제목 : ");
 				String title = sc.nextLine();
-				System.out.print("내용 ) ");
+				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-				
+
 				Article article = new Article(id, title, body);
 				articles.add(article);
-				
-				System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
+
+				System.out.println(id + "번 글이 생성되었습니다");
 				lastArticleId++;
-			} else if(cmd.equals("article list")) {
-				System.out.println("게시글 목록");
-				
+			} else if (cmd.equals("article list")) {
+				System.out.println("==게시물 목록==");
+
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+					continue;
+				}
+
+				System.out.println("번호   /   제목");
+
+				for (Article article : articles) {
+					System.out.printf("%d   /   %s\n",article.id, article.title);
+				}
 			}
+
 		}
-		System.out.println("프로그램 종료");
+
+		System.out.println("==프로그램 종료==");
 		sc.close();
 	}
-
 }
